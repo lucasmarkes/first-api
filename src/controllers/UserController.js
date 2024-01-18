24338d6155ead6ab017ad0fd12c8d2577bedef14
Lucas Marques <lama@cesar.org.br>
2024-01-18 17:41:28 -0300

    Add UserController with listUsers and getUserById methods

---
+++ src/controllers/UserController.js
@@ -0,0 +1,30 @@
+// Controller -> responsável por armazenar toda regra de negócios da aplicação
+const users = require("../mocks/users");
+
+module.exports = {
+  listUsers(req, res) {
+    const { order } = req.query;
+
+    const sortedUsers = users.sort((a, b) => {
+      if (order === "desc") {
+        return a.id < b.id ? 1 : -1;
+      }
+
+      return a.id > b.id ? 1 : -1;
+    });
+
+    res.send(200, sortedUsers);
+  },
+
+  getUserById(req, res) {
+    const { id } = req.params;
+
+    const user = users.find((user) => user.id === Number(id));
+
+    if (!user) {
+      return res.send(400, { error: "User not found." });
+    }
+
+    res.send(200, user);
+  },
+};
