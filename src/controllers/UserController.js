2c4bf768fb2ad218cd1dfa4e0aacb30e5bbdb0f9
Lucas Marques <lama@cesar.org.br>
2024-01-27 15:17:03 -0300

    feat: added create user method

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -27,4 +27,26 @@ module.exports = {
 
     res.send(200, user);
   },
+
+  createUser(req, res) {
+    let body = "";
+
+    req.on("data", (chunk) => {
+      body += chunk;
+    });
+
+    req.on("end", () => {
+      body = JSON.parse(body);
+
+      const lastUserId = users[users.length - 1].id;
+
+      const newUser = {
+        id: lastUserId + 1,
+        name: body.name,
+      };
+
+      users.push(newUser);
+      res.send(200, newUser);
+    });
+  },
 };
